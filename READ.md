7307bb23f223d5bcd967aac10757abfd7c99ff16
Kuddleman <edl1155@gmail.com>
2021-01-10 12:48:14 -0800

    scaffolding for post and bodyparser

--- READ.md
+++ READ.md
@@ -1,2 +1,7 @@
 Git hub repo:
 https://github.com/zellwk/crud-demo?ck_subscriber_id=1136051781
+
+
+With the script I added in package.json, I can also start nodemon
+by running in the command line:
+npm run dev
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+
+// Express doesn't handle reading data on its own  
+//Need body parser.  
+//Body Parser is middleware
+const bodyParser = require('body-parser')
+
 const app = express() 
 const PORT = 3000
 
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res)=>{
     res.sendFile(`${__dirname}/index.html`)
+})
+
+app.post('/quotes', (req, res)=>{
+    console.log(`This is from ANOTHER POST REQUEST`)
 })
 
 
